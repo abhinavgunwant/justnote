1b0b20c0bac91877c5cc4d546c6977d49f555b73
Abhinav Gunwant <abhigunwant@gmail.com>
2025-06-15 04:55:46 +0530

    feat: vault index entry type convert to & from u8

--- types/src/vault_index_entry.rs
+++ types/src/vault_index_entry.rs
@@ -1,8 +1,28 @@
+use std::convert::{ From, Into };
+
 #[derive(Debug, Default, PartialEq, Clone)]
 pub enum VaultIndexEntryType {
     #[default]
     Note,
     Folder,
+}
+
+impl From<u8> for VaultIndexEntryType {
+    fn from(num: u8) -> Self {
+        match num {
+            1 => VaultIndexEntryType::Folder,
+            _ => VaultIndexEntryType::Note,
+        }
+    }
+}
+
+impl Into<u8> for VaultIndexEntryType {
+    fn into(self) -> u8 {
+        match self {
+            VaultIndexEntryType::Note => 0,
+            VaultIndexEntryType::Folder => 1,
+        }
+    }
 }
 
 #[derive(Debug, Default, PartialEq, Clone)]
